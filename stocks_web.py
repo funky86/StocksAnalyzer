edf6b7d97735849bfb043935cdb6953aa13ae035
Tilen Bobek <tilen.bobek@gmail.com>
2017-10-23 21:28:40 +0200

    Page stocks - don't die in case of no http parameters. Minor css styling.

--- stocks_web.py
+++ stocks_web.py
@@ -13,7 +13,12 @@ def stocks():
     import stocks
     symbols = request.args.get('symbols')
     data = []
-    for symbol in symbols.rsplit(','):
-        stock_data = stocks.get_values(symbol)
-        data.append(stock_data)
+    if symbols:
+	    for symbol in symbols.rsplit(','):
+	        stock_data = stocks.get_values(symbol)
+	        data.append(stock_data)
     return render_template('stocks.html', stocks_data=data)
+
+@app.route('/queries')
+def queries():
+	return render_template('queries.html')
